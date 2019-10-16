1a040eca4cfff775596b17aa06a32953ca7530c1
tancongcong123 <tancongcong_123@163.com>
2019-10-16 09:18:42 +0800

    practice

--- app/src/main/java/com/example/algorithms/linkedList/ListNode.java
+++ app/src/main/java/com/example/algorithms/linkedList/ListNode.java
@@ -10,7 +10,7 @@ public class ListNode {
         return next;
     }
 
-    ListNode(int x) {
+    public ListNode(int x) {
         val = x;
     }
 
---
+++ app/src/main/java/com/example/algorithms/practice/AddTwoNumbers.java
@@ -0,0 +1,56 @@
+package com.example.algorithms.practice;
+
+import com.example.algorithms.linkedList.ListNode;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args){
+        System.out.println();
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1==null&&l2==null)
+            return null;
+        if (l1==null)
+            return l2;
+        if (l2==null)
+            return l1;
+        int remainder = 0;
+        ListNode head = null;
+        ListNode preNode = null;
+        ListNode currentNode = null;
+        int currentVal = 0;
+        while (l1!=null || l2!=null){
+            if (l1==null){
+                currentVal = l2.val+remainder;
+                l2 = l2.next;
+            }else if (l2==null){
+                currentVal = l1.val+remainder;
+                l1 = l1.next;
+            }else {
+                currentVal = l1.val+l2.val+remainder;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            if (currentVal>9){
+                remainder = 1;
+                currentVal = currentVal-10;
+            }else {
+                remainder = 0;
+            }
+            currentNode = new ListNode(currentVal);
+            if (head==null){
+                head = currentNode;
+            }
+            if (preNode!=null){
+                preNode.next = currentNode;
+            }
+            preNode = currentNode;
+        }
+        if (remainder==1){
+            currentNode = new ListNode(1);
+            preNode.next = currentNode;
+        }
+        return head;
+    }
+}
